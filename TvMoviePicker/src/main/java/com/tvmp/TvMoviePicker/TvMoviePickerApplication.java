d4c646227491f63043daf46f23111a579bf8d62c
James Richardson <jdr@jamesdrichardson.dev>
2023-10-20 16:45:05 -0400

    please work

---
+++ TvMoviePicker/src/main/java/com/tvmp/TvMoviePicker/TvMoviePickerApplication.java
@@ -0,0 +1,13 @@
+package com.tvmp.TvMoviePicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvMoviePickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvMoviePickerApplication.class, args);
+	}
+
+}
